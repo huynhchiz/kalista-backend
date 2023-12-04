f19d3f43aeb4b9636ab0be43ba0204c4adf87b8e
huynhchiz <huynhchi90524@gmail.com>
2023-12-04 22:11:53 +0700

    socket chat

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ import express from "express";
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 
+// import { Server } from 'socket.io';
+import http from 'http'
+
 import configCors from "./config/configCors";
 
 import accountRoutes from './routes/account.js';
@@ -10,16 +13,23 @@ import userRoutes from './routes/user.js';
 import postRoutes from './routes/post.js';
 import messageRoutes from './routes/message.js';
 
+
+
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3333
 
+const server = http.createServer(app)
+// const io = new Server(server)
+const io = require("socket.io")(server, {
+  cors: {
+      origin: "*",
+  }
+}); 
+
 // config CORS
 configCors(app);
-// config body-parser
-// app.use(bodyParser.urlencoded({ extended: true, limit: "25mb" }));
-// app.use(bodyParser.json({ limit: "25mb" }));
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(
@@ -33,14 +43,22 @@ app.use(
 // config cookie parser
 app.use(cookieParser());
 
-// set limit url image
-// app.use(express.json({ limit: "50mb" }));
-// app.use(express.urlencoded({ limit: "50mb" }));
-
 accountRoutes(app)
 userRoutes(app)
 postRoutes(app)
 messageRoutes(app)
+
+io.on('connection', (socket) => { // Handle khi có connect từ client tới
+  console.log('user connected ' + io.id);
+
+  socket.on("sendDataClient", (data) => { // Handle khi có sự kiện tên là sendDataClient từ phía client
+    io.emit("sendDataServer", { data });// phát sự kiện  có tên sendDataServer cùng với dữ liệu tin nhắn từ phía server
+  })
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected"); // Khi client disconnect thì log ra terminal.
+  });
+})
 
 app.use((req, res) => {
     res.send('404 not found!')
@@ -48,4 +66,8 @@ app.use((req, res) => {
 
 app.listen(PORT, () => {
     console.log(`kalista-backend listening on port ${PORT}`);
+})
+
+server.listen(3434, () => {
+  console.log(`kalista-chat server listening on port ${3434}`);
 })
